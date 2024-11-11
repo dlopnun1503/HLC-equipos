40eea39968abc7ea229afeed81c594483ed25efd
dlopnun1503 <dlopnun1503@g.educaand.es>
2024-11-11 14:28:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'equipos',
+  webDir: 'www'
+};
+
+export default config;
